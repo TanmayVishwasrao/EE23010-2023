65bdfd523ab92f9b70940a9802d403cad529cb4d
Tanmay <tanmayvishwasrao@gmail.com>
2023-08-05 21:32:55 +0530

    Updated

---
+++ codes/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy import linalg as LA
+A=np.array([1,-1])
+B=np.array([-4,6])
+C=np.array([-3,-5])
+a=np.linalg.norm(C-B)
+b=np.linalg.norm(C-A)
+c=np.linalg.norm(A-B)
+I=np.array([(a*A[0]+b*B[0]+c*C[0])/(a+b+c),(a*A[1]+b*B[1]+c*C[1])/(a+b+c)])
+print("I = ",I)
+E=A-B
+F=A-C
+k1=((I-A)@(A-B))/((A-B)@(A-B))
+E3=A+(k1*(A-B))
+k2=((I-A)@(A-C))/((A-C)@(A-C))
+F3=A+(k2*(A-C))
+print("k1 = ",k1)
+print("k2 = ",k2)
+print("E3 = ",E3)
+print("F3 = ",F3)
