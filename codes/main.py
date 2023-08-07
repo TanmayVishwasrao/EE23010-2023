e9cdbb57fc5193961af48c8077f5ee8a5846b4c7
Tanmay <tanmayvishwasrao@gmail.com>
2023-08-07 13:02:03 +0530

    updated

--- codes/main.py
+++ codes/main.py
@@ -3,13 +3,14 @@ from numpy import linalg as LA
 A=np.array([1,-1])
 B=np.array([-4,6])
 C=np.array([-3,-5])
+#finding the side lengths
 a=np.linalg.norm(C-B)
 b=np.linalg.norm(C-A)
 c=np.linalg.norm(A-B)
+#Incentre
 I=np.array([(a*A[0]+b*B[0]+c*C[0])/(a+b+c),(a*A[1]+b*B[1]+c*C[1])/(a+b+c)])
 print("I = ",I)
-E=A-B
-F=A-C
+
 k1=((I-A)@(A-B))/((A-B)@(A-B))
 E3=A+(k1*(A-B))
 k2=((I-A)@(A-C))/((A-C)@(A-C))
@@ -18,3 +19,4 @@ print("k1 = ",k1)
 print("k2 = ",k2)
 print("E3 = ",E3)
 print("F3 = ",F3)
+
