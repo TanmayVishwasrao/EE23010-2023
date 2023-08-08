8483e0e233940ee270a9c590405ad47b32d6ab67
Tanmay <tanmayvishwasrao@gmail.com>
2023-08-08 16:30:51 +0530

    updated

--- codes/main.py
+++ codes/main.py
@@ -10,10 +10,11 @@ c=np.linalg.norm(A-B)
 #Incentre
 I=np.array([(a*A[0]+b*B[0]+c*C[0])/(a+b+c),(a*A[1]+b*B[1]+c*C[1])/(a+b+c)])
 print("I = ",I)
-
+#finding k for E_3 and F_3
 k1=((I-A)@(A-B))/((A-B)@(A-B))
-E3=A+(k1*(A-B))
 k2=((I-A)@(A-C))/((A-C)@(A-C))
+#finding E_3 and F_3
+E3=A+(k1*(A-B))
 F3=A+(k2*(A-C))
 print("k1 = ",k1)
 print("k2 = ",k2)
